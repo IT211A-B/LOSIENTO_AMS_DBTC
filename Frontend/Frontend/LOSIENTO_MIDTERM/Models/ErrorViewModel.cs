c6010aa7157cba155b27ab553368bd880e8ced58
Kebs-deck <johnkevin.villacorte@dbtc-cebu.edu.ph>
2026-03-27 14:20:23 +0800

    Remove Frontend and Backend folders

--- Frontend/Frontend/LOSIENTO_MIDTERM/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LOSIENTO_MIDTERM.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
